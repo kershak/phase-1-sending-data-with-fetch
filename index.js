7ad5d86a8829735df9051ead7a805a1bc24bc322
Nills <yakusaz17@gmail.com>
2024-08-19 18:49:16 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+
+
+function submitData (name, email) {
+    return fetch ("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then (res => res.json())
+    .then (object => document.body.innerHTML = object["id"])
+    .catch (error => document.body.innerHTML = error.message)
+}
+  
+    
